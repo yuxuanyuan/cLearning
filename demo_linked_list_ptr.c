9ecd300f2fa869434a4585f69993bcd44337dc39
Yuxuan Yuan <39542705+yuxuanyuan@users.noreply.github.com>
2020-04-05 21:25:35 +0800

    add an insert function

--- demo_linked_list_ptr.c
+++ demo_linked_list_ptr.c
@@ -4,20 +4,22 @@
 #include <stdio.h>
 #include <string.h>
 
+#define MAX 128 // max length of a string
+
 /* typedef a structure named Dict (dictionary)
  * In the dictionary, you have items with keys and values
- * Here I define the key is a integer and the value is
+ * Here I define the key is an integer and the value is
  * a pointer
  * The code here can be used to:
  *  1) create a dict
  *  2) print a dict
  *  3) add an item
- *  4) delete an item
- *  5) reverse all items and
+ *  4) delete an item and
+ *  5) reverse all items
  *  6) destory the dict
  *
- * Note: When key == -1, the program completes.
- */
+ * Note: When key == -1, the program stops.
+*/
 
 /* TODO:
 *  1) How to catch the stdin with whitespaces
@@ -27,17 +29,17 @@ typedef struct Node{
     int Key;
     char *Value;
     struct Node *pNext;
-}Dict_t;
+}dict_t;
 
-Dict_t *Dict_Create() {
-    Dict_t *pHead, *pCurrent, *pNew;
+dict_t *Dict_Create() {
+    dict_t *pHead, *pCurrent, *pNew;
     int key;
     char *value;
 
     value = (char *)malloc(sizeof(char));
     if (value == NULL) return NULL;
 
-    pHead = (Dict_t *) malloc(sizeof(Dict_t));
+    pHead = (dict_t *) malloc(sizeof(dict_t));
     if (pHead == NULL) return NULL;
 
     pHead->Key = 0;
@@ -54,7 +56,7 @@ Dict_t *Dict_Create() {
 
     pCurrent = pHead;
     while (key != -1) {
-        pNew = (Dict_t *) malloc(sizeof(Dict_t));
+        pNew = (dict_t *) malloc(sizeof(dict_t));
         if (pNew == NULL) return NULL;
         pNew->Key = key;
         pNew->Value = value;
@@ -72,8 +74,9 @@ Dict_t *Dict_Create() {
     }
     return pHead;
 }
-int Dict_Print(Dict_t *pHead){
-    Dict_t *tmp;
+
+int Dict_Print(dict_t *pHead){
+    dict_t *tmp;
     if (pHead == NULL) return -1;
     tmp = pHead->pNext;
     printf("\nBegin\n\tThe dict is:\n\t{\n");
@@ -85,10 +88,44 @@ int Dict_Print(Dict_t *pHead){
     return 0;
 }
 
+int Item_insert(dict_t *pHead, int tgtKey, int Key, char *Value){
+    dict_t *pCurrent, *pPrevious, *pNew;
+
+    pNew = (dict_t *)malloc(sizeof(dict_t));
+    if (pNew  == NULL) return -1;
+
+    pNew->Key = Key;
+    pNew->Value = Value;
+    pNew->pNext = NULL;
+
+    pPrevious = pHead;
+    pCurrent = pHead->pNext; //pPre->next
+
+    while (pCurrent)
+    {
+        if (pCurrent->Key == tgtKey) {
+            break;
+        }
+        if (pCurrent->Key == Key){
+            pCurrent->Value = Value;
+            goto END;
+        }
+        pPrevious = pCurrent;
+        pCurrent = pCurrent->pNext;
+    }
+    pNew ->pNext = pPrevious->pNext;
+    pPrevious->pNext = pNew;
+    goto END;
+    END:
+        return 0;
+}
+
 int main(){
     int ret = 0;
-    Dict_t *pHead = NULL;
+    dict_t *pHead = NULL;
     pHead = Dict_Create();
+    ret = Item_insert(pHead, 20, 10, "test");
     ret = Dict_Print(pHead);
+
     return ret;
 }
